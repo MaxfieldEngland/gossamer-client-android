1975926d06ad760b280ba37102ea1337d9d39294
Elijah <elijahff@uw.edu>
2020-07-26 16:00:31 -0700

    Updated app logo with custom gossamer logo

---
+++ app/src/main/java/edu/tacoma/uw/gossamer_client_android/authenticate/SignInActivity.java
@@ -0,0 +1,15 @@
+package edu.tacoma.uw.gossamer_client_android.authenticate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import edu.tacoma.uw.gossamer_client_android.R;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
