7b60d92117941c41d1e1b2c02a15a5c24ba523dc
Elijah <elijahff@uw.edu>
2020-07-26 18:30:24 -0700

    Added SharedPreferences functionality

--- app/src/main/java/edu/tacoma/uw/gossamer_client_android/authenticate/SignInActivity.java
+++ app/src/main/java/edu/tacoma/uw/gossamer_client_android/authenticate/SignInActivity.java
@@ -4,30 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import edu.tacoma.uw.gossamer_client_android.R;
 import edu.tacoma.uw.gossamer_client_android.home.MainActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class SignInActivity extends AppCompatActivity implements LoginFragment.LoginFragmentListener {
+
+    private SharedPreferences mSharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.sign_in_fragment_id, new LoginFragment())
-                .commit();
+        mSharedPreferences = getSharedPreferences(getString(R.string.LOGIN_PREFS)
+                                    , Context.MODE_PRIVATE);
+
+        if (!mSharedPreferences.getBoolean(getString(R.string.LOGGEDIN), false)) {
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.sign_in_fragment_id, new LoginFragment())
+                    .commit();
+        } else {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 
 
 
     public void login(String email, String pwd) {
-
         //TODO - Validate Email and Password against user info stored in database.
-        Toast.makeText(this, "Clicked Login Button", Toast.LENGTH_SHORT).show();
 
+        mSharedPreferences
+                .edit()
+                .putBoolean(getString(R.string.LOGGEDIN), true)
+                .apply();
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
