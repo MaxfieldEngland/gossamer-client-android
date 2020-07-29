60ed039778f7d0f564addb9c9b9f26f406e5238c
Elijah <elijahff@uw.edu>
2020-07-29 11:42:58 -0700

    Added validate password functionality for Register Fragment

--- app/src/main/java/edu/tacoma/uw/gossamer_client_android/authenticate/RegisterFragment.java
+++ app/src/main/java/edu/tacoma/uw/gossamer_client_android/authenticate/RegisterFragment.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import edu.tacoma.uw.gossamer_client_android.R;
 
@@ -70,19 +71,30 @@ public class RegisterFragment extends Fragment {
         final EditText userNameEText = view.findViewById(R.id.username_edit_text);
         final EditText emailEText = view.findViewById(R.id.email_edit_text);
         final EditText pwdEText = view.findViewById(R.id.password_edit_text);
+        final EditText pwdConfirmEText=view.findViewById(R.id.confirm_pwd_edit_text);
+
         final Button registerButton = view.findViewById(R.id.registration_button);
         //Adds functionality to button and sends data to SignInActivity.
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO - Validate the username, email, & password.
                 String userName = userNameEText.getText().toString();
                 String email = emailEText.getText().toString();
                 String pwd = pwdEText.getText().toString();
-                //Sends information to the SignInActivity.
-                mRegisterFragmentListener.register(userName, email, pwd);
+                String confirmPwd = pwdConfirmEText.getText().toString();
+
+                if (!validatePassword(pwd, confirmPwd)) {
+                    Toast.makeText(getContext(), "Password does not match",Toast.LENGTH_SHORT).show();
+                } else { //Sends information to the SignInActivity.
+                    mRegisterFragmentListener.register(userName, email, pwd);
+                }
+
             }
         });
         return view;
+    }
+
+    private boolean validatePassword(String pwd, String confirmPwd) {
+        return pwd.equals(confirmPwd);
     }
 }
