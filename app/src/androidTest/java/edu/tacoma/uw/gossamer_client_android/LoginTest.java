ee2ada36a87316519fdc8eed9095548dd2902482
MaxfieldEngland <maxengl@uw.edu>
2020-08-17 19:02:19 -0700

    Margin fix

---
+++ app/src/androidTest/java/edu/tacoma/uw/gossamer_client_android/LoginTest.java
@@ -0,0 +1,12 @@
+package edu.tacoma.uw.gossamer_client_android;
+
+import org.junit.Rule;
+
+import edu.tacoma.uw.gossamer_client_android.authenticate.LoginFragment;
+import edu.tacoma.uw.gossamer_client_android.authenticate.SignInActivity;
+
+public class LoginTest {
+
+    @Rule
+    public ActivityTestRule<SignInActivity> mActivityRule = new ActivityTestRule<>(SignInActivity.class);
+}
